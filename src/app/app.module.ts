a0c7a8b2ef3c693c74653beaca36c50157420a53
davirshark <davirodrigo2524@gmail.com>
2022-07-05 00:26:51 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConverterComponent } from './converter/converter.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConverterComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/converter/converter.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.css']
+})
+export class ConverterComponent implements OnInit {
+
+  @Input("contentToConvert") contentToConvert = {} as HTMLElement
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  generatePDF() {
+    console.log(this.contentToConvert)
+    html2canvas(this.contentToConvert).then(canvas => {
+      console.log(canvas)
+      var imgWidth = 208;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png')
+      var doc = new jsPDF("p", "mm", "a4");
+      var position = 0;
+      doc.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      doc.save('newPDF.pdf');
+    }).finally ( () => {
+      console.log ("O fim da promessa sendo erro ou não")
+    });
+  }
+}
